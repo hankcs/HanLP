870b481749130873b1d5b921037409f52a1e39d0
hankcs <jfservice@126.com>
2015-07-04 16:18:09 +0800

    防御性代码

--- src/main/java/com/hankcs/hanlp/seg/Segment.java
+++ src/main/java/com/hankcs/hanlp/seg/Segment.java
@@ -291,6 +291,7 @@ public abstract class Segment
      */
     public List<Term> seg(String text)
     {
+        if (text == null || text.length() == 0) return Collections.emptyList();
         char[] charArray = text.toCharArray();
         if (HanLP.Config.Normalization)
         {
