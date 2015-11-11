43b8d3e2e27c6f1fffdb76016d86e2d986e2034b
hankcs <jfservice@126.com>
2015-11-11 19:13:24 +0800

    修正WordNatureDependencyModel

--- src/main/java/com/hankcs/hanlp/model/bigram/WordNatureDependencyModel.java
+++ src/main/java/com/hankcs/hanlp/model/bigram/WordNatureDependencyModel.java
@@ -105,12 +105,11 @@ public class WordNatureDependencyModel
 
     static boolean saveDat(String path, TreeMap<String, Attribute> map)
     {
-        if (!trie.save(path + Predefine.TRIE_EXT)) return false;
         Collection<Attribute> attributeList = map.values();
         // 缓存值文件
         try
         {
-            DataOutputStream out = new DataOutputStream(new FileOutputStream(path + Predefine.VALUE_EXT));
+            DataOutputStream out = new DataOutputStream(new FileOutputStream(path + Predefine.BIN_EXT));
             out.writeInt(attributeList.size());
             for (Attribute attribute : attributeList)
             {
@@ -126,6 +125,7 @@ public class WordNatureDependencyModel
                     out.writeFloat(attribute.p[i]);
                 }
             }
+            if (!trie.save(out)) return false;
             out.close();
         }
         catch (Exception e)
@@ -138,7 +138,7 @@ public class WordNatureDependencyModel
 
     static boolean loadDat(String path)
     {
-        ByteArray byteArray = ByteArray.createByteArray(path + Predefine.VALUE_EXT);
+        ByteArray byteArray = ByteArray.createByteArray(path + Predefine.BIN_EXT);
         if (byteArray == null) return false;
         int size = byteArray.nextInt();
         Attribute[] attributeArray = new Attribute[size];
@@ -154,7 +154,7 @@ public class WordNatureDependencyModel
             attributeArray[i] = attribute;
         }
 
-        return trie.load(path + Predefine.TRIE_EXT, attributeArray);
+        return trie.load(byteArray, attributeArray);
     }
 
     public static Attribute get(String key)
