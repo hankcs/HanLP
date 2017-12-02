5ed83a538b3ba04762de14076e4017a951fca3ba
hankcs <jfservice@126.com>
2017-12-01 22:02:34 -0600

    使word2vec命令行参数解析与原版兼容：https://github.com/hankcs/HanLP/issues/699

--- src/main/java/com/hankcs/hanlp/mining/word2vec/AbstractTrainer.java
+++ src/main/java/com/hankcs/hanlp/mining/word2vec/AbstractTrainer.java
@@ -59,12 +59,12 @@ public abstract class AbstractTrainer
         int i;
         if ((i = argPos("-size", args)) >= 0) config.setLayer1Size(Integer.parseInt(args[i + 1]));
         if ((i = argPos("-output", args)) >= 0) config.setOutputFile(args[i + 1]);
-        if ((i = argPos("-cbow", args)) >= 0) config.setUseContinuousBagOfWords(true);
+        if ((i = argPos("-cbow", args)) >= 0) config.setUseContinuousBagOfWords(Integer.parseInt(args[i + 1]) == 1);
         if (config.useContinuousBagOfWords()) config.setAlpha(0.05f);
         if ((i = argPos("-alpha", args)) >= 0) config.setAlpha(Float.parseFloat(args[i + 1]));
         if ((i = argPos("-window", args)) >= 0) config.setWindow(Integer.parseInt(args[i + 1]));
         if ((i = argPos("-sample", args)) >= 0) config.setSample(Float.parseFloat(args[i + 1]));
-        if ((i = argPos("-hs", args)) >= 0) config.setUseHierarchicalSoftmax(true);
+        if ((i = argPos("-hs", args)) >= 0) config.setUseHierarchicalSoftmax(Integer.parseInt(args[i + 1]) == 1);
         if ((i = argPos("-negative", args)) >= 0) config.setNegative(Integer.parseInt(args[i + 1]));
         if ((i = argPos("-threads", args)) >= 0) config.setNumThreads(Integer.parseInt(args[i + 1]));
         if ((i = argPos("-iter", args)) >= 0) config.setIter(Integer.parseInt(args[i + 1]));
