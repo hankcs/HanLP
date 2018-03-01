4c34e1d7d30c30ac2669ee7b678df00a609e3f3d
hankcs <jfservice@126.com>
2018-02-28 23:06:30 -0600

    :triangular_flag_on_post: 可变双数组trie树

---
+++ src/main/java/com/hankcs/hanlp/collection/trie/datrie/package-info.java
@@ -0,0 +1,14 @@
+/*
+ * <author>Hankcs</author>
+ * <email>me@hankcs.com</email>
+ * <create-date>2018-02-28 下午9:17</create-date>
+ *
+ * <copyright file="package-info.java" company="码农场">
+ * Copyright (c) 2018, 码农场. All Right Reserved, http://www.hankcs.com/
+ * This source is subject to Hankcs. Please contact Hankcs to get more information.
+ * </copyright>
+ */
+/**
+ * 可变双数组trie树，可以当做Map<String, V>来用。如果V是int，可以直接用MutableDoubleArrayTrieInteger
+ */
+package com.hankcs.hanlp.collection.trie.datrie;
