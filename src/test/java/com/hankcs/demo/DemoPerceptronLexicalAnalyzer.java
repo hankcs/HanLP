d633b49d4f65802fdd587e4cab0f2160f95bc75c
hankcs <jfservice@126.com>
2018-03-16 13:25:49 -0500

    示例感知机词法分析器在线学习功能

--- src/test/java/com/hankcs/demo/DemoPerceptronLexicalAnalyzer.java
+++ src/test/java/com/hankcs/demo/DemoPerceptronLexicalAnalyzer.java
@@ -25,5 +25,20 @@ public class DemoPerceptronLexicalAnalyzer
     {
         PerceptronLexicalAnalyzer analyzer = new PerceptronLexicalAnalyzer();
         System.out.println(analyzer.analyze("上海华安工业（集团）公司董事长谭旭光和秘书胡花蕊来到美国纽约现代艺术博物馆参观"));
+
+        // 任何模型总会有失误，特别是98年这种陈旧的语料库
+        System.out.println(analyzer.analyze("总统普京与特朗普通电话讨论太空探索技术公司"));
+        // 支持在线学习
+        analyzer.learn("与/c 特朗普/nr 通/v 电话/n 讨论/v [太空/s 探索/vn 技术/n 公司/n]/nt");
+        // 学习到新知识
+        System.out.println(analyzer.analyze("总统普京与特朗普通电话讨论太空探索技术公司"));
+        // 还可以举一反三
+        System.out.println(analyzer.analyze("主席和特朗普通电话"));
+
+        // 知识的泛化不是死板的规则，而是比较灵活的统计信息
+        System.out.println(analyzer.analyze("我在浙江金华出生"));
+        analyzer.learn("在/p 浙江/ns 金华/ns 出生/v");
+        System.out.println(analyzer.analyze("我在浙江金华出生"));
+        System.out.println(analyzer.analyze("我的名字叫金华"));
     }
 }
