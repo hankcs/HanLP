49c72d2ce269e75b6a78c098eda953795f656613
hankcs <jfservice@126.com>
2018-03-25 18:03:07 -0500

    为Sentence添加一些接口

--- src/main/java/com/hankcs/hanlp/corpus/document/sentence/Sentence.java
+++ src/main/java/com/hankcs/hanlp/corpus/document/sentence/Sentence.java
@@ -119,6 +119,21 @@ public class Sentence implements Serializable, Iterable<IWord>
         return this;
     }
 
+    /**
+     * 按照 PartOfSpeechTagDictionary 指定的映射表将复合词词语词性翻译过去
+     *
+     * @return
+     */
+    public Sentence translateCompoundWordLabels()
+    {
+        for (IWord word : wordList)
+        {
+            if (word instanceof CompoundWord)
+                word.setLabel(PartOfSpeechTagDictionary.translate(word.getLabel()));
+        }
+        return this;
+    }
+
     private void printWord(IWord word, StringBuilder sb, int id, int offset)
     {
         char delimiter = '\t';
