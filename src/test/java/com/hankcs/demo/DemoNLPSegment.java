76e455955aee6ae82661c0388eb4d1eb4aceab0a
hankcs <jfservice@126.com>
2018-04-04 11:31:27 -0500

    演示NLP分词

--- src/test/java/com/hankcs/demo/DemoNLPSegment.java
+++ src/test/java/com/hankcs/demo/DemoNLPSegment.java
@@ -14,7 +14,9 @@ package com.hankcs.demo;
 import com.hankcs.hanlp.tokenizer.NLPTokenizer;
 
 /**
- * NLP分词
+ * NLP分词，更精准的中文分词、词性标注与命名实体识别
+ * 标注集请查阅 https://github.com/hankcs/HanLP/blob/master/data/dictionary/other/TagPKU98.csv
+ * 或者干脆调用 Sentence#translateLabels() 转为中文
  *
  * @author hankcs
  */
@@ -22,8 +24,9 @@ public class DemoNLPSegment
 {
     public static void main(String[] args)
     {
-        System.out.println(NLPTokenizer.segment("我新造一个词叫幻想乡你能识别并标注正确词性吗？"));
-        // 注意观察两个“希望”的词性，两个“晚霞”的词性
+        System.out.println(NLPTokenizer.segment("我新造一个词叫幻想乡你能识别并正确标注词性吗？")); // “正确”是副形词。
+        // 注意观察下面两个“希望”的词性、两个“晚霞”的词性
         System.out.println(NLPTokenizer.analyze("我的希望是希望张晚霞的背影被晚霞映红").translateLabels());
+        System.out.println(NLPTokenizer.analyze("支援臺灣正體香港繁體：微软公司於1975年由比爾·蓋茲和保羅·艾倫創立。"));
     }
 }
