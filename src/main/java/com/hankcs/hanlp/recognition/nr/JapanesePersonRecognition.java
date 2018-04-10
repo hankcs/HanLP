a8efe05cdbb46b1b3695f7942a23c7019ae773f1
hankcs <jfservice@126.com>
2018-04-09 23:37:07 -0500

    优化日本人名识别

--- src/main/java/com/hankcs/hanlp/recognition/nr/JapanesePersonRecognition.java
+++ src/main/java/com/hankcs/hanlp/recognition/nr/JapanesePersonRecognition.java
@@ -11,6 +11,7 @@
  */
 package com.hankcs.hanlp.recognition.nr;
 
+import com.hankcs.hanlp.collection.trie.DoubleArrayTrie;
 import com.hankcs.hanlp.corpus.tag.Nature;
 import com.hankcs.hanlp.dictionary.BaseSearcher;
 import com.hankcs.hanlp.dictionary.CoreDictionary;
@@ -44,15 +45,14 @@ public class JapanesePersonRecognition
         StringBuilder sbName = new StringBuilder();
         int appendTimes = 0;
         char[] charArray = wordNetAll.charArray;
-        BaseSearcher searcher = JapanesePersonDictionary.getSearcher(charArray);
-        Map.Entry<String, Character> entry;
+        DoubleArrayTrie<Character>.LongestSearcher searcher = JapanesePersonDictionary.getSearcher(charArray);
         int activeLine = 1;
         int preOffset = 0;
-        while ((entry = searcher.next()) != null)
+        while (searcher.next())
         {
-            Character label = entry.getValue();
-            String key = entry.getKey();
-            int offset = searcher.getOffset();
+            Character label = searcher.value;
+            int offset = searcher.begin;
+            String key = new String(charArray, offset, searcher.length);
             if (preOffset != offset)
             {
                 if (appendTimes > 1 && sbName.length() > 2) // 日本人名最短为3字
