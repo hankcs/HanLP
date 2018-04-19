c5ca15fff49b1399cc67318de33e221c88dfe0e9
hankcs <jfservice@126.com>
2018-04-19 14:08:30 -0500

    修复词法分析器禁用用户词典时发生的问题

--- src/main/java/com/hankcs/hanlp/tokenizer/lexical/AbstractLexicalAnalyzer.java
+++ src/main/java/com/hankcs/hanlp/tokenizer/lexical/AbstractLexicalAnalyzer.java
@@ -306,10 +306,10 @@ public abstract class AbstractLexicalAnalyzer extends CharacterBasedSegment impl
                 }
                 String[] posArray = tag(wordArray);
                 Iterator<Term> iterator = termList.iterator();
-                Iterator<CoreDictionary.Attribute> attributeIterator = attributeList.iterator();
+                Iterator<CoreDictionary.Attribute> attributeIterator = attributeList == null ? null : attributeList.iterator();
                 for (String pos : posArray)
                 {
-                    if (attributeIterator.hasNext())
+                    if (attributeIterator != null && attributeIterator.hasNext())
                     {
                         CoreDictionary.Attribute attribute = attributeIterator.next();
                         if (attribute != null)
--- src/test/java/com/hankcs/hanlp/model/perceptron/PerceptronSegmenterTest.java
+++ src/test/java/com/hankcs/hanlp/model/perceptron/PerceptronSegmenterTest.java
@@ -1,5 +1,6 @@
 package com.hankcs.hanlp.model.perceptron;
 
+import com.hankcs.hanlp.dictionary.CustomDictionary;
 import junit.framework.TestCase;
 
 import java.util.List;
@@ -25,5 +26,13 @@ public class PerceptronSegmenterTest extends TestCase
         String text = "他们确保了唐纳德·特朗普在总统大选中获胜。";
         List<String> wordList = segmenter.segment(text);
         assertTrue(wordList.contains("唐纳德·特朗普"));
+    }
+
+    public void testNoCustomDictionary() throws Exception
+    {
+        PerceptronLexicalAnalyzer analyzer = new PerceptronLexicalAnalyzer();
+        analyzer.enableCustomDictionary(false);
+        CustomDictionary.insert("禁用用户词典");
+        assertEquals("[禁用/v, 用户/n, 词典/n]", analyzer.seg("禁用用户词典").toString());
     }
 }
