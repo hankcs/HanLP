4b112bf704f5ef6d4ca19b61012bff65c71462f1
hankcs <jfservice@126.com>
2018-05-28 15:51:32 +0800

    AC自动机支持从字符串集合构造

--- src/main/java/com/hankcs/hanlp/algorithm/ahocorasick/trie/Trie.java
+++ src/main/java/com/hankcs/hanlp/algorithm/ahocorasick/trie/Trie.java
@@ -37,6 +37,11 @@ public class Trie
         this(new TrieConfig());
     }
 
+    public Trie(Collection<String> keywords)
+    {
+        this();
+        addAllKeyword(keywords);
+    }
 
     public Trie removeOverlaps()
     {
