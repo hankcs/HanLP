71cef539f3e1d5a4b64d25d53c64a760421ea59c
hankcs <jfservice@126.com>
2018-06-22 16:07:47 +0800

    文档

--- src/main/java/com/hankcs/hanlp/model/perceptron/model/StructuredPerceptron.java
+++ src/main/java/com/hankcs/hanlp/model/perceptron/model/StructuredPerceptron.java
@@ -44,11 +44,11 @@ public class StructuredPerceptron extends LinearModel
         {
             if (goldIndex[i] == predictIndex[i])
                 continue;
-            else
+            else // 预测与答案不一致
             {
-                parameter[goldIndex[i]]++;
+                parameter[goldIndex[i]]++; // 奖励正确的特征函数（将它的权值加一）
                 if (predictIndex[i] >= 0 && predictIndex[i] < parameter.length)
-                    parameter[predictIndex[i]]--;
+                    parameter[predictIndex[i]]--; // 惩罚招致错误的特征函数（将它的权值减一）
                 else
                 {
                     throw new IllegalArgumentException("更新参数时传入了非法的下标");
@@ -57,6 +57,11 @@ public class StructuredPerceptron extends LinearModel
         }
     }
 
+    /**
+     * 在线学习
+     *
+     * @param instance 样本
+     */
     public void update(Instance instance)
     {
         int[] guessLabel = new int[instance.length()];
@@ -65,8 +70,8 @@ public class StructuredPerceptron extends LinearModel
         for (int i = 0; i < instance.length(); i++)
         {
             int[] featureVector = instance.getFeatureAt(i);
-            int[] goldFeature = new int[featureVector.length];
-            int[] predFeature = new int[featureVector.length];
+            int[] goldFeature = new int[featureVector.length]; // 根据答案应当被激活的特征
+            int[] predFeature = new int[featureVector.length]; // 实际预测时激活的特征
             for (int j = 0; j < featureVector.length - 1; j++)
             {
                 goldFeature[j] = featureVector[j] * tagSet.size() + instance.tagArray[i];
