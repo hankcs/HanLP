64c6d11fea15c1b5a2747a16b62d3f2205943ccf
hankcs <jfservice@126.com>
2018-06-24 10:33:33 +0800

    自动下载分词语料库

--- src/test/java/com/hankcs/hanlp/model/perceptron/DemoEvaluateCWS.java
+++ src/test/java/com/hankcs/hanlp/model/perceptron/DemoEvaluateCWS.java
@@ -11,15 +11,15 @@ public class DemoEvaluateCWS
     {
         logger.start("开始训练...\n");
         PerceptronTrainer trainer = new CWSTrainer();
-        PerceptronTrainer.Result result = trainer.train("data/test/msr_training.utf8", "data/test/msr_test_gold.utf8", "data/test/msr_cws.bin",
+        PerceptronTrainer.Result result = trainer.train(MSR.TRAIN_PATH, MSR.GOLD_PATH, MSR.MODEL_PATH,
                                                         0.0, // 压缩比对准确率的影响很小
-                                                        100, // 一般5个迭代就差不多收敛了
-                                                        Runtime.getRuntime().availableProcessors() // 单线程的平均感知机算法收敛更稳定
+                                                        50, // 一般5个迭代就差不多收敛了
+                                                        1 // 单线程的平均感知机算法收敛更稳定
         );
         logger.finish(" 训练完毕\n");
 
         double[] prf = result.prf;
         System.out.printf("P: %.2f R: %.2f F1: %.2f\n", prf[0], prf[1], prf[2]);
-        // P: 96.41 R: 96.25 F1: 96.33
+        // P: 96.69 R: 96.38 F1: 96.53
     }
 }
---
+++ src/test/java/com/hankcs/hanlp/model/perceptron/MSR.java
@@ -0,0 +1,37 @@
+/*
+ * <author>Han He</author>
+ * <email>me@hankcs.com</email>
+ * <create-date>2018-06-24 10:34 AM</create-date>
+ *
+ * <copyright file="MSR.java">
+ * Copyright (c) 2018, Han He. All Rights Reserved, http://www.hankcs.com/
+ * This source is subject to Han He. Please contact Han He for more information.
+ * </copyright>
+ */
+package com.hankcs.hanlp.model.perceptron;
+
+import com.hankcs.hanlp.corpus.io.IOUtil;
+import com.hankcs.hanlp.utility.TestUtility;
+
+/**
+ * @author hankcs
+ */
+public class MSR
+{
+    public static String TRAIN_PATH = "data/test/icwb2-data/training/msr_training.utf8";
+    public static String TEST_PATH = "data/test/icwb2-data/testing/msr_test.utf8";
+    public static String GOLD_PATH = "data/test/icwb2-data/gold/msr_test_gold.utf8";
+    public static String MODEL_PATH = "data/test/msr_cws";
+    public static String OUTPUT_PATH = "data/test/msr_output.txt";
+    public static String TRAIN_WORDS = "data/test/icwb2-data/gold/msr_training_words.utf8";
+
+    static
+    {
+        String sighan05 = TestUtility.ensureData("icwb2-data", "http://sighan.cs.uchicago.edu/bakeoff2005/data/icwb2-data.zip");
+        if (!IOUtil.isFileExisted(TRAIN_PATH))
+        {
+            System.err.println("请下载 http://sighan.cs.uchicago.edu/bakeoff2005/data/icwb2-data.zip 并解压为 data/test/icwb2-data");
+            System.exit(1);
+        }
+    }
+}
