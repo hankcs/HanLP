0421b87e8e54a604975db0a31d1fa186bc6f9079
hankcs <jfservice@126.com>
2019-01-27 10:32:21 -0500

    tokenizer的seg2sentence修改为static

--- src/main/java/com/hankcs/hanlp/tokenizer/NotionalTokenizer.java
+++ src/main/java/com/hankcs/hanlp/tokenizer/NotionalTokenizer.java
@@ -89,7 +89,7 @@ public class NotionalTokenizer
      * @param shortest 是否断句为最细的子句（将逗号也视作分隔符）
      * @return 句子列表，每个句子由一个单词列表组成
      */
-    public List<List<Term>> seg2sentence(String text, boolean shortest)
+    public static List<List<Term>> seg2sentence(String text, boolean shortest)
     {
         return SEGMENT.seg2sentence(text, shortest);
     }
