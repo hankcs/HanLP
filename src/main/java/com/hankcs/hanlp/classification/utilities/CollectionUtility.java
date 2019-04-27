a538d0722ab2e4980a9dcd9ea40324fc3ddba7ce
hankcs <jfservice@126.com>
2019-04-27 18:08:11 -0400

    修正 CollectionUtility.sortMapByValue(java.util.Map<K,V>, boolean) fix https://github.com/hankcs/HanLP/issues/1159

--- src/main/java/com/hankcs/hanlp/classification/utilities/CollectionUtility.java
+++ src/main/java/com/hankcs/hanlp/classification/utilities/CollectionUtility.java
@@ -21,7 +21,7 @@ public class CollectionUtility
     public static <K, V extends Comparable<V>> Map<K, V> sortMapByValue(Map<K, V> input, final boolean desc)
     {
         LinkedHashMap<K, V> output = new LinkedHashMap<K, V>(input.size());
-        ArrayList<Map.Entry<K, V>> entryList = new ArrayList<Map.Entry<K, V>>(input.size());
+        ArrayList<Map.Entry<K, V>> entryList = new ArrayList<Map.Entry<K, V>>(input.entrySet());
         Collections.sort(entryList, new Comparator<Map.Entry<K, V>>()
         {
             public int compare(Map.Entry<K, V> o1, Map.Entry<K, V> o2)
