1ff8b764c9094bb5f5b73fa4d5d37969623e9206
hankcs <jfservice@126.com>
2020-01-01 17:53:31 -0500

    PTB biaffine parsing model UAS: 0.9594 - LAS: 0.9430

--- hanlp/pretrained/dep.py
+++ hanlp/pretrained/dep.py
@@ -5,3 +5,4 @@ from hanlp.common.constant import HANLP_URL
 
 CTB5_BIAFFINE_DEP = HANLP_URL + 'dep/biaffine_ctb5_20191229_025833.zip'
 CTB7_BIAFFINE_DEP = HANLP_URL + 'dep/biaffine_ctb7_20191229_130325.zip'
+PTB_BIAFFINE_DEP = HANLP_URL + 'dep/ptb_dep_biaffine_20200101_174624.zip'
