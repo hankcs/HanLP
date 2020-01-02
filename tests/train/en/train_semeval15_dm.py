d82b496c8d288b10457c652ce73c862aa6cedb0c
hankcs <jfservice@126.com>
2020-01-01 20:53:16 -0500

    train PTB semantic parsing

---
+++ tests/train/en/train_semeval15_dm.py
@@ -0,0 +1,26 @@
+# -*- coding:utf-8 -*-
+# Author: hankcs
+# Date: 2020-01-01 18:26
+from hanlp.components.parsers.biaffine_parser import BiaffineSemanticDependencyParser
+from hanlp.pretrained.glove import GLOVE_6B_100D
+from tests import cdroot
+
+cdroot()
+save_dir = 'data/model/sdp/semeval15_biaffine_dm'
+parser = BiaffineSemanticDependencyParser()
+parser.fit('data/semeval15/en.dm.train.conll', 'data/semeval15/en.dm.dev.conll', save_dir,
+           pretrained_embed={'class_name': 'HanLP>Word2VecEmbedding',
+                             'config': {
+                                 'trainable': False,
+                                 'embeddings_initializer': 'zero',
+                                 'filepath': GLOVE_6B_100D,
+                                 'expand_vocab': True,
+                                 'lowercase': True,
+                                 'normalize': True,
+                             }},
+           )
+sentence = [('Is', 'VBZ'), ('this', 'DT'), ('the', 'DT'), ('future', 'NN'), ('of', 'IN'), ('chamber', 'NN'),
+            ('music', 'NN'), ('?', '.')]
+print(parser.predict(sentence))
+parser.evaluate('data/semeval15/en.id.dm.auto.conllu', save_dir)
+parser.evaluate('data/semeval15/en.ood.dm.auto.conllu', save_dir)
---
+++ tests/train/en/train_semeval15_psd.py
@@ -0,0 +1,26 @@
+# -*- coding:utf-8 -*-
+# Author: hankcs
+# Date: 2020-01-01 18:26
+from hanlp.components.parsers.biaffine_parser import BiaffineSemanticDependencyParser
+from hanlp.pretrained.glove import GLOVE_6B_100D
+from tests import cdroot
+
+cdroot()
+save_dir = 'data/model/sdp/semeval15_biaffine_psd'
+parser = BiaffineSemanticDependencyParser()
+parser.fit('data/semeval15/en.psd.train.conll', 'data/semeval15/en.psd.dev.conll', save_dir,
+           pretrained_embed={'class_name': 'HanLP>Word2VecEmbedding',
+                             'config': {
+                                 'trainable': False,
+                                 'embeddings_initializer': 'zero',
+                                 'filepath': GLOVE_6B_100D,
+                                 'expand_vocab': True,
+                                 'lowercase': True,
+                                 'normalize': True,
+                             }},
+           )
+sentence = [('Is', 'VBZ'), ('this', 'DT'), ('the', 'DT'), ('future', 'NN'), ('of', 'IN'), ('chamber', 'NN'),
+            ('music', 'NN'), ('?', '.')]
+print(parser.predict(sentence))
+parser.evaluate('data/semeval15/en.id.psd.conll', save_dir)
+parser.evaluate('data/semeval15/en.ood.psd.conll', save_dir)
