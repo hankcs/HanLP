b7605fa4e357d01834c3c09cf2b93182b4e769c1
hankcs <jfservice@126.com>
2020-01-02 19:00:59 -0500

    tuning conv cws

--- tests/train/zh/cws/train_pku980106_conv_cws.py
+++ tests/train/zh/cws/train_pku980106_conv_cws.py
@@ -4,18 +4,17 @@
 import tensorflow as tf
 
 from hanlp.components.tok import NgramConvTokenizer
-from hanlp.datasets.cws.sighan2005.pku import SIGHAN2005_PKU_TEST, SIGHAN2005_PKU_VALID
 from hanlp.pretrained.word2vec import RADICAL_CHAR_EMBEDDING_100
 from tests import cdroot
 
 cdroot()
 
 tokenizer = NgramConvTokenizer()
-save_dir = 'data/model/cws/sighan2005-pku-convseg'
+save_dir = 'data/model/cws/pku98_6m_conv_ngram'
 optimizer = tf.keras.optimizers.Adam(learning_rate=0.001,
                                      epsilon=1e-8, clipnorm=5)
 tokenizer.fit('data/cws/pku98/199801-06-seg.txt',
-              SIGHAN2005_PKU_VALID,
+              'data/cws/pku98/test_pku98_name_merged.txt',
               save_dir,
               word_embed={'class_name': 'HanLP>Word2VecEmbedding',
                           'config': {
@@ -26,7 +25,8 @@ tokenizer.fit('data/cws/pku98/199801-06-seg.txt',
                           }},
               optimizer=optimizer,
               window_size=0,
+              epochs=1,
               weight_norm=True)
-tokenizer.evaluate(SIGHAN2005_PKU_TEST, save_dir=save_dir, output=False)
+tokenizer.evaluate('data/cws/pku98/test_pku98_name_merged.txt', save_dir=save_dir, output=False)
 print(tokenizer.predict(['中央民族乐团离开北京前往维也纳', '商品和服务']))
 print(f'Model saved in {save_dir}')
