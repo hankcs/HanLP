33b8cdeb1b3070133f1cc83a4d4d538a2ddf033a
hankcs <jfservice@126.com>
2020-01-03 15:48:48 -0500

    English SDP model UF: 0.9437 - LF: 0.9362 - speed: 428.29 sample/sec

--- hanlp/pretrained/sdp.py
+++ hanlp/pretrained/sdp.py
@@ -6,4 +6,6 @@ from hanlp.common.constant import HANLP_URL
 SEMEVAL16_NEWS_BIAFFINE = HANLP_URL + 'sdp/semeval16-news-biaffine_20191231_235407.zip'
 SEMEVAL16_TEXT_BIAFFINE = HANLP_URL + 'sdp/semeval16-text-biaffine_20200101_002257.zip'
 
+SEMEVAL15_EN_PAS_BIAFFINE = HANLP_URL + 'sdp/semeval15_biaffine_pas_20200103_152405.zip'
+
 ALL = {}
---
+++ tests/demo/en/demo_sdp.py
@@ -0,0 +1,17 @@
+# -*- coding:utf-8 -*-
+# Author: hankcs
+# Date: 2020-01-03 15:26
+import hanlp
+from hanlp.components.parsers.conll import CoNLLSentence
+
+semantic_parser = hanlp.load(hanlp.pretrained.sdp.SEMEVAL15_EN_PAS_BIAFFINE)
+sent = [('Is', 'VBZ'),
+        ('this', 'DT'),
+        ('the', 'DT'),
+        ('future', 'NN'),
+        ('of', 'IN'),
+        ('chamber', 'NN'),
+        ('music', 'NN'),
+        ('?', '.')]
+tree = semantic_parser.predict(sent)  # type:CoNLLSentence
+print(tree)
