ea9211d38273b1e64d82e40640260f3d55e54473
hankcs <jfservice@126.com>
2020-01-03 22:40:31 -0500

    English NER model

--- hanlp/pretrained/ner.py
+++ hanlp/pretrained/ner.py
@@ -4,5 +4,6 @@
 from hanlp.common.constant import HANLP_URL
 
 MSRA_NER_BERT_BASE_ZH = HANLP_URL + 'ner/ner_bert_base_msra_20191230_205748.zip'
+CONLL03_NER_BERT_BASE_UNCASED_EN = HANLP_URL + 'ner/ner_conll03_bert_base_uncased_en_20200101_175841.zip'
 
 ALL = {}
