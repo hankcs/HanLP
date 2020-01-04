7b146d074ffec6e48ce091f2fb2601e8414930b1
hankcs <jfservice@126.com>
2020-01-04 16:51:52 -0500

    release BERT sentiment classifier on chnsenticorp, accuracy: 0.9567 - speed: 147.37 sample/sec

--- hanlp/pretrained/classifiers.py
+++ hanlp/pretrained/classifiers.py
@@ -3,6 +3,6 @@
 # Date: 2020-01-01 03:51
 from hanlp.common.constant import HANLP_URL
 
-CHNSENTICORP_BERT_BASE = HANLP_URL + 'classification/chnsenticorp_bert_base_20200104_163711.zip'
+CHNSENTICORP_BERT_BASE = HANLP_URL + 'classification/chnsenticorp_bert_base_20200104_164655.zip'
 
 ALL = {}
