a6086fdbcde2e43c1e0a3154676ea574dc0e3f41
hankcs <jfservice@126.com>
2020-01-04 18:09:28 -0500

    release BERT sentiment classifier on SST2, accuracy: 0.9281 - speed: 179.10 sample/sec

--- hanlp/pretrained/classifiers.py
+++ hanlp/pretrained/classifiers.py
@@ -3,6 +3,7 @@
 # Date: 2020-01-01 03:51
 from hanlp.common.constant import HANLP_URL
 
-CHNSENTICORP_BERT_BASE = HANLP_URL + 'classification/chnsenticorp_bert_base_20200104_164655.zip'
+CHNSENTICORP_BERT_BASE_ZH = HANLP_URL + 'classification/chnsenticorp_bert_base_20200104_164655.zip'
+SST2_BERT_BASE_EN = HANLP_URL + 'classification/sst2_bert_base_uncased_en_20200104_175422.zip'
 
 ALL = {}
