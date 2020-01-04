353db5bf435815b2830329857b9262e7db012c1a
hankcs <jfservice@126.com>
2020-01-03 23:52:29 -0500

    release the conv model trained on PKU 6 months

--- hanlp/pretrained/cws.py
+++ hanlp/pretrained/cws.py
@@ -8,6 +8,7 @@ SIGHAN2005_MSR_CONVSEG = HANLP_URL + 'cws/sighan2005-msr-convseg_20191229_014345
 SIGHAN2005_MSR_BERT_BASE = HANLP_URL + 'cws/cws_bert_base_msra_20191230_194627.zip'
 CTB6_CONVSEG = HANLP_URL + 'cws/ctb6-convseg-cws_20191230_184525.zip'
 CTB6_BERT_BASE = HANLP_URL + 'cws/cws_bert_base_ctb6_20191230_185536.zip'
+PKU_NAME_MERGED_SIX_MONTHS_CONVSEG = HANLP_URL + 'cws/pku98_6m_conv_ngram_20200103_232809.zip'
 
 # Will be filled up during runtime
 ALL = {}
--- tests/demo/zh/demo_cws.py
+++ tests/demo/zh/demo_cws.py
@@ -3,7 +3,7 @@
 # Date: 2019-12-28 21:25
 import hanlp
 
-tokenizer = hanlp.load('CTB6_CONVSEG')
+tokenizer = hanlp.load('PKU_NAME_MERGED_SIX_MONTHS_CONVSEG')
 print(tokenizer.predict('商品和服务'))
 print(tokenizer.predict(['萨哈夫说，伊拉克将同联合国销毁伊拉克大规模杀伤性武器特别委员会继续保持合作。',
                          '上海华安工业（集团）公司董事长谭旭光和秘书张晚霞来到美国纽约现代艺术博物馆参观。']))
