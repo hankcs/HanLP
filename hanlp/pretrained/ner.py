51ef7233fee6585660551fc1b0118c566c402e83
hankcs <jfservice@126.com>
2020-01-04 20:48:13 -0500

    new BERT CoNLL03 NER, twice faster, f1: 0.9135 - speed: 68.44 sample/sec

--- hanlp/pretrained/ner.py
+++ hanlp/pretrained/ner.py
@@ -5,7 +5,7 @@ from hanlp.common.constant import HANLP_URL
 
 MSRA_NER_BERT_BASE_ZH = HANLP_URL + 'ner/ner_bert_base_msra_20200104_185735.zip'
 MSRA_NER_ALBERT_BASE_ZH = HANLP_URL + 'ner/ner_albert_base_msra_20200104_183033.zip'
-CONLL03_NER_BERT_BASE_UNCASED_EN = HANLP_URL + 'ner/ner_conll03_bert_base_uncased_en_20200101_175841.zip'
+CONLL03_NER_BERT_BASE_UNCASED_EN = HANLP_URL + 'ner/ner_conll03_bert_base_uncased_en_20200104_194352.zip'
 CONLL03_NER_BERT_BASE_CASED_EN = HANLP_URL + 'ner/ner_conll03_bert_base_cased_en_20200103_234420.zip'
 
 ALL = {}
