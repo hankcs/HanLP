b4f13a213b32544a930657cfa5650700ed1cfe89
hankcs <jfservice@126.com>
2020-01-11 20:30:46 -0500

    release new albert ner model

--- hanlp/pretrained/ner.py
+++ hanlp/pretrained/ner.py
@@ -4,7 +4,7 @@
 from hanlp.common.constant import HANLP_URL
 
 MSRA_NER_BERT_BASE_ZH = HANLP_URL + 'ner/ner_bert_base_msra_20200104_185735.zip'
-MSRA_NER_ALBERT_BASE_ZH = HANLP_URL + 'ner/ner_albert_base_msra_20200104_183033.zip'
+MSRA_NER_ALBERT_BASE_ZH = HANLP_URL + 'ner/ner_albert_base_zh_msra_20200111_202919.zip'
 CONLL03_NER_BERT_BASE_UNCASED_EN = HANLP_URL + 'ner/ner_conll03_bert_base_uncased_en_20200104_194352.zip'
 
 ALL = {}
