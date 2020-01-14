2666c80a8b47410ba762f702218448a1a679fd35
hankcs <jfservice@126.com>
2020-01-14 00:03:04 -0500

    release new pos model fix https://github.com/hankcs/HanLP/issues/1403

--- hanlp/pretrained/pos.py
+++ hanlp/pretrained/pos.py
@@ -3,7 +3,7 @@
 # Date: 2019-12-29 01:57
 from hanlp.common.constant import HANLP_URL
 
-CTB5_POS_RNN = HANLP_URL + 'pos/ctb5_pos_rnn_20191229_015325.zip'
+CTB5_POS_RNN = HANLP_URL + 'pos/ctb5_pos_rnn_20200113_235925.zip'
 CTB5_POS_RNN_FASTTEXT_ZH = HANLP_URL + 'pos/ctb5_pos_rnn_fasttext_20191230_202639.zip'
 
 PTB_POS_RNN_FASTTEXT_EN = HANLP_URL + 'pos/ptb_pos_rnn_fasttext_20200103_145337.zip'
