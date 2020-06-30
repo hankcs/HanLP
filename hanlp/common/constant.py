6c566a6b992fe3562e7cef0366ce4d5de3f3b808
hankcs <jfservice@126.com>
2020-06-30 10:10:47 -0400

    Support mirror site through environment variable HANLP_URL

--- hanlp/common/constant.py
+++ hanlp/common/constant.py
@@ -1,8 +1,9 @@
 # -*- coding:utf-8 -*-
 # Author: hankcs
 # Date: 2019-06-13 22:41
+import os
 
 PAD = '<pad>'
 UNK = '<unk>'
 ROOT = '<root>'
-HANLP_URL = 'https://file.hankcs.com/hanlp/'
+HANLP_URL = os.getenv('HANLP_URL', 'https://file.hankcs.com/hanlp/')
