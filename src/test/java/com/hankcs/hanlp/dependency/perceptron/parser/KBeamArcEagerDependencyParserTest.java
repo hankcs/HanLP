66e328d1d66bedeb41ed91134eecb38a38f95805
hankcs <jfservice@126.com>
2020-11-15 11:36:13 -0500

    新增 KBeamArcEagerDependencyParser(String modelPath, String cwsModelPath, String posModelPath) fix https://github.com/hankcs/HanLP/issues/1585

---
+++ src/test/java/com/hankcs/hanlp/dependency/perceptron/parser/KBeamArcEagerDependencyParserTest.java
@@ -0,0 +1,13 @@
+package com.hankcs.hanlp.dependency.perceptron.parser;
+
+import junit.framework.TestCase;
+
+import java.io.IOException;
+
+public class KBeamArcEagerDependencyParserTest extends TestCase
+{
+    public void testLoad() throws IOException, ClassNotFoundException
+    {
+        KBeamArcEagerDependencyParser parser = new KBeamArcEagerDependencyParser();
+    }
+}
