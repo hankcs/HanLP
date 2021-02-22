a1db48ce0c317efda25e9863dfe8c455a5c86330
hankcs <jfservice@126.com>
2021-02-22 12:59:57 -0500

    Test small close model with multithreading as it's more powerful

--- tests/test_mtl.py
+++ tests/test_mtl.py
@@ -5,22 +5,22 @@ from hanlp_common.document import Document
 
 
 def tokenize(mtl, text):
-    return mtl(text, tasks='tok')['tok']
+    return mtl(text, tasks='tok/fine')['tok/fine']
 
 
 class TestMultiTaskLearning(unittest.TestCase):
 
     def setUp(self) -> None:
         super().setUp()
-        self.mtl = hanlp.load(hanlp.pretrained.mtl.OPEN_TOK_POS_NER_SRL_DEP_SDP_CON_ELECTRA_SMALL_ZH, devices=-1)
+        self.mtl = hanlp.load(hanlp.pretrained.mtl.CLOSE_TOK_POS_NER_SRL_DEP_SDP_CON_ELECTRA_SMALL_ZH, devices=-1)
 
     def test_mtl_single_sent(self):
         doc: Document = self.mtl('商品和服务')
-        self.assertSequenceEqual(doc['tok'], ["商品", "和", "服务"])
+        self.assertSequenceEqual(doc['tok/fine'], ["商品", "和", "服务"])
 
     def test_mtl_multiple_sents(self):
         doc: Document = self.mtl(['商品和服务', '研究生命'])
-        self.assertSequenceEqual(doc['tok'], [
+        self.assertSequenceEqual(doc['tok/fine'], [
             ["商品", "和", "服务"],
             ["研究", "生命"]
         ])
