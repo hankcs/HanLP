19fcda3decc2847028f405d1491346a21a354094
hankcs <jfservice@126.com>
2021-05-18 00:11:34 -0400

    Revise demo

--- plugins/hanlp_demo/hanlp_demo/ja/demo_mtl.py
+++ plugins/hanlp_demo/hanlp_demo/ja/demo_mtl.py
@@ -4,7 +4,7 @@
 import hanlp
 from hanlp_common.document import Document
 
-HanLP = hanlp.load(hanlp.pretrained.mtl.NPCMJ_UD_KYOTO_TOK_POS_CON_BERT_BASE_CHAR_JA, devices=-1)
+HanLP = hanlp.load(hanlp.pretrained.mtl.NPCMJ_UD_KYOTO_TOK_POS_CON_BERT_BASE_CHAR_JA)
 doc: Document = HanLP([
     '2021年、HanLPv2.1は次世代の最先端多言語NLP技術を本番環境に導入します。',
     '奈須きのこは1973年11月28日に千葉県円空山で生まれ、ゲーム制作会社「ノーツ」の設立者だ。',
