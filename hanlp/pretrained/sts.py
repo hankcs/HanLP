41d8654bf3b55722bc85a4eb80fbb8bb7c38c8d8
hankcs <jfservice@126.com>
2021-05-29 01:25:04 -0400

    Update the STS model

--- hanlp/pretrained/sts.py
+++ hanlp/pretrained/sts.py
@@ -3,7 +3,7 @@
 # Date: 2021-05-24 12:51
 from hanlp_common.constant import HANLP_URL
 
-STS_ELECTRA_BASE_ZH = HANLP_URL + 'sts/sts_electra_base_zh_20210524_130550.zip'
+STS_ELECTRA_BASE_ZH = HANLP_URL + 'sts/sts_electra_base_zh_20210530_200109.zip'
 'A naive regression model trained on concatenated STS corpora.'
 
 # Will be filled up during runtime
