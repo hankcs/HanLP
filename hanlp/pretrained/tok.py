e8f62eece6b0d074d40a25fe992c2028ab5c8da3
hankcs <jfservice@126.com>
2021-06-03 11:25:58 -0400

    Release a new tok model COARSE_ELECTRA_SMALL_ZH

--- hanlp/pretrained/tok.py
+++ hanlp/pretrained/tok.py
@@ -17,6 +17,9 @@ LARGE_ALBERT_BASE = HANLP_URL + 'tok/large_cws_albert_base_20200828_011451.zip'
 'ALBERT model (:cite:`Lan2020ALBERT:`) trained on the largest CWS dataset in the world.'
 SIGHAN2005_PKU_BERT_BASE_ZH = HANLP_URL + 'tok/sighan2005_pku_bert_base_zh_20201231_141130.zip'
 'BERT model (:cite:`devlin-etal-2019-bert`) trained on sighan2005 pku dataset.'
+COARSE_ELECTRA_SMALL_ZH = HANLP_URL + 'tok/coarse_electra_small_zh_20210603_112321.zip'
+'Electra small model trained on coarse-grained CWS corpora. Its performance is much higher than that of MTL ' \
+'model: P: 97.08% R: 96.94% F1: 97.01%'
 
 # Will be filled up during runtime
 ALL = {}
