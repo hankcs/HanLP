bf5cf5663f9f36c18a836d173537b6bc43e813db
hankcs <jfservice@126.com>
2021-12-28 19:30:21 -0500

    Fix en pipeline

--- plugins/hanlp_demo/hanlp_demo/en/demo_pipeline.py
+++ plugins/hanlp_demo/hanlp_demo/en/demo_pipeline.py
@@ -2,8 +2,9 @@
 # Author: hankcs
 # Date: 2020-01-04 21:05
 import hanlp
+from hanlp.utils.lang.en.english_tokenizer import tokenize_english
 
-tokenizer = hanlp.utils.rules.tokenize_english
+tokenizer = tokenize_english
 tagger = hanlp.load(hanlp.pretrained.pos.PTB_POS_RNN_FASTTEXT_EN)
 syntactic_parser = hanlp.load(hanlp.pretrained.dep.PTB_BIAFFINE_DEP_EN)
 semantic_parser = hanlp.load(hanlp.pretrained.sdp.SEMEVAL15_PAS_BIAFFINE_EN)
@@ -12,8 +13,10 @@ pipeline = hanlp.pipeline() \
     .append(hanlp.utils.rules.split_sentence, output_key='sentences') \
     .append(tokenizer, output_key='tokens') \
     .append(tagger, output_key='part_of_speech_tags') \
-    .append(syntactic_parser, input_key=('tokens', 'part_of_speech_tags'), output_key='syntactic_dependencies', conll=False) \
-    .append(semantic_parser, input_key=('tokens', 'part_of_speech_tags'), output_key='semantic_dependencies', conll=False)
+    .append(syntactic_parser, input_key=('tokens', 'part_of_speech_tags'), output_key='syntactic_dependencies',
+            conll=False) \
+    .append(semantic_parser, input_key=('tokens', 'part_of_speech_tags'), output_key='semantic_dependencies',
+            conll=False)
 print(pipeline)
 
 text = '''Jobs and Wozniak co-founded Apple in 1976 to sell Wozniak's Apple I personal computer.
