17fb9f7cd1168788070ad2973814b782b4f773b7
hankcs <jfservice@126.com>
2022-01-18 18:47:44 -0500

    Release a CTB9 pos model

--- hanlp/pretrained/pos.py
+++ hanlp/pretrained/pos.py
@@ -11,6 +11,8 @@ CTB9_POS_ALBERT_BASE = HANLP_URL + 'pos/ctb9_albert_base_20211228_163935.zip'
 'ALBERT model (:cite:`Lan2020ALBERT:`) trained on CTB9 (:cite:`https://doi.org/10.35111/gvd0-xk91`). This is a TF component.'
 CTB9_POS_ELECTRA_SMALL_TF = HANLP_URL + 'pos/pos_ctb_electra_small_20211227_121341.zip'
 'Electra small model (:cite:`clark2020electra`) trained on CTB9 (:cite:`https://doi.org/10.35111/gvd0-xk91`). Accuracy = `96.75`. This is a TF component.'
+CTB9_POS_ELECTRA_SMALL = HANLP_URL + 'pos/ctb9_pos_electra_small_20220118_164341.zip'
+'Electra small model (:cite:`clark2020electra`) trained on CTB9 (:cite:`https://doi.org/10.35111/gvd0-xk91`). Accuracy = `96.62`.'
 
 C863_POS_ELECTRA_SMALL = HANLP_URL + 'pos/pos_863_electra_small_20210808_124848.zip'
 'Electra small model (:cite:`clark2020electra`) trained on Chinese 863 corpus. Accuracy = `95.22`.'
