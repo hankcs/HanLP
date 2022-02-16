3df547533bfc578ba77725235211de11cb088d9e
hankcs <jfservice@126.com>
2022-02-16 10:06:25 -0500

    Release a dep model trained on PKU Multi-view Chinese Treebank (PMT)

--- hanlp/pretrained/dep.py
+++ hanlp/pretrained/dep.py
@@ -7,9 +7,12 @@ CTB5_BIAFFINE_DEP_ZH = HANLP_URL + 'dep/biaffine_ctb5_20191229_025833.zip'
 'Biaffine LSTM model (:cite:`dozat:17a`) trained on CTB5.'
 CTB7_BIAFFINE_DEP_ZH = HANLP_URL + 'dep/biaffine_ctb7_20200109_022431.zip'
 'Biaffine LSTM model (:cite:`dozat:17a`) trained on CTB7.'
-CTB9_DEP_ELECTRA_SMALL = HANLP_URL + 'dep/ctb9_dep_electra_small_20220204_221541.zip'
+CTB9_DEP_ELECTRA_SMALL = HANLP_URL + 'dep/ctb9_dep_electra_small_20220216_100306.zip'
 'Electra small encoder (:cite:`clark2020electra`) with Biaffine decoder (:cite:`dozat:17a`) trained on CTB9-SD330. ' \
-'Performance is UAS=87.74% LAS=83.67%.'
+'Performance is UAS=87.68% LAS=83.54%.'
+PMT1_DEP_ELECTRA_SMALL = HANLP_URL + 'dep/pmt_dep_electra_small_20220218_134518.zip'
+'Electra small encoder (:cite:`clark2020electra`) with Biaffine decoder (:cite:`dozat:17a`) trained on PKU ' \
+'Multi-view Chinese Treebank (PMT) 1.0 (:cite:`qiu-etal-2014-multi`). Performance is UAS=87.68% LAS=83.54%.'
 
 PTB_BIAFFINE_DEP_EN = HANLP_URL + 'dep/ptb_dep_biaffine_20200101_174624.zip'
 'Biaffine LSTM model (:cite:`dozat:17a`) trained on PTB.'
