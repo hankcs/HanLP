fbfc8fb23055a61f4812fe31ed7926c35a0e5630
hankcs <jfservice@126.com>
2022-12-07 16:33:47 -0500

    Add support for Python 3.10

--- setup.py
+++ setup.py
@@ -47,6 +47,7 @@ setup(
         'Programming Language :: Python :: 3.7',
         'Programming Language :: Python :: 3.8',
         'Programming Language :: Python :: 3.9',
+        'Programming Language :: Python :: 3.10',
         'Topic :: Scientific/Engineering :: Artificial Intelligence',
         "Topic :: Text Processing :: Linguistic"
     ],
