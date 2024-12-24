75edc60c6f431562a41e9ce4b1b56d74d4497624
hankcs <jfservice@126.com>
2024-12-24 00:52:45 -0800

    Update eos model config to support new lines

--- hanlp/pretrained/eos.py
+++ hanlp/pretrained/eos.py
@@ -3,7 +3,7 @@
 # Date: 2020-12-22 13:22
 from hanlp_common.constant import HANLP_URL
 
-UD_CTB_EOS_MUL = HANLP_URL + 'eos/eos_ud_ctb_mul_20201222_133543.zip'
+UD_CTB_EOS_MUL = HANLP_URL + 'eos/eos_ud_ctb_mul_20241224_165114.zip'
 'EOS model (:cite:`Schweter:Ahmed:2019`) trained on concatenated UD2.3 and CTB9.'
 
 # Will be filled up during runtime
